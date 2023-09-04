2fe6ec19a4595b8f5522660a5b15e6f96603a958
saif120991 <saif120991@gmail.com>
2023-09-05 02:16:41 +0530

    js linking,comments, semicolon,consolestrings ways

---
+++ 02-stringMethods.js
@@ -0,0 +1,57 @@
+//to print in browser console, we use console.log 
+
+// console.log("hello world");
+
+//--------------------------how to  link  js file in html--------------------------------------
+
+//hamesha js file ko body me ek dum last me link karna agar head tag ke andar karoge toh defer likhna jaruri hai 
+
+// ex:-
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>javascript_notes</title>
+    //    <script src="02-index.js" defer></script>
+// </head>
+// <body>
+//     <!-- <script src="01-historyOfJavascript.js"></script> -->
+//     <script src="02-index.js"></script>
+// </body>
+// </html>
+
+//----------------different methods to  print string in console--------------------------------
+
+/*hum 3 tarike se string ko  print kara sakte hai single quotes or double quotes or back ticks (back ticks string templates me use hota hai),generally double quotes use karte hai apne upar hai  single bhi use kar sakte hai jo mann kare
+
+console.log("double quotes");
+console.log('single quotes');
+console.log(`back ticks`);
+*/
+
+
+//--------------------------semicolon(;)----------------------------------
+
+//semicolon is  optional  in js script aap nhi lagaoge tab bhi js chal jayegi but semi colon lagana ek achhi practice mani jati hai
+
+
+//------------------------------comments--------------------------------------------
+
+/*
+comments hum kuch apne code me likhna  chahte hai ki ye code kya karta hai aur hum ye bhi chahte hai ki ye js engine execute na kare, ye info uske lie hai jo mera code dekehega kaise code run karna hai usse he hum comments kehte hai basically 2 type pe comment hote hai 
+
+
+1)single line comment 
+// 2 forward slash likh ke jo likhoge wo single line comment hoga (shortcut:- ctrl+/)
+
+2)multi line comment
+ /*forward slash ke baad star fhir star 
+ fhir slash inke beech me hum 
+ jitni line chahhe likh sakte hai isse 
+ multi line comment kehte hai (shortcut:- ctrl+/)*/
+
+
+
+
+*/
