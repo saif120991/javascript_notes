907d68ddb4ff98540b4aff6036772b7ad56cc251
saif120991 <saif120991@gmail.com>
2023-09-05 01:24:34 +0530

    My Personal Notes Of Javascript

---
+++ 01-historyOfJavascript.js
@@ -0,0 +1,24 @@
+//1995 Me Brandan Eich ne jo ki netscape navigator browser company me kaam karte the unhone 10 din me javascript ko banaya 
+
+//iska sabse pehle naam mocha rakha gya tha fhir badal ke livescript rakha gya phir popularity ke lie naam javascript rakh dia gya 
+
+//javascript backward compatible hai matlab aaj ke browser me 20 saal purana code bhi chal jayega
+
+//javascript forward compatible nhi hai matlab 20 saal purane browser pe aaj ka code nhi chalega.
+
+//javascript ke standards means rules features ye sab ECMA decide karti hai , TC-39 ek team hai jo decide karti hai kya features
+
+//javascript ka trademark oracle ke pass hai isliye ECMA ne isse ECMA script kaha ,dono sahi hai chahe hum ecma script bole ya javascript
+
+
+//ecma ka pehla version aaya javascript ka jisko kehte hai
+// ES1:1997 
+
+//ES5:2009 
+//bohot sare new features lekar ECMA aayi 2009 me  jisse javascript ko log use karne lage
+
+//ES6:2015 (modern javascript)
+//isse hum modern javascript bhi kehte hai aur isse biggest update in the history of javascript bhi kaha jata hai
+
+
+//babel ek javascript compiler hai jo modern javascript ke code ko purane codes me  convert kar deta hai jisse janha purane codes likhe gaye hai wanha pe bhi js easily implement ho jaye
