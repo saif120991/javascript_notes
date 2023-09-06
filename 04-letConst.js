c05ab61ae648983d91d516e6a4e9c6a4f671028d
saif120991 <saif120991@gmail.com>
2023-09-06 18:13:51 +0530

    js notes

---
+++ 04-letConst.js
@@ -0,0 +1,20 @@
+//-------------let keyword-------------------
+
+//declare variable  with let keyword
+// let firstName = "king arthur";
+// firstName = "queen elizabeth";
+// console.log(firstName);
+
+//in let keyword we cannot redeclare same variable with same name it will through error
+
+//----------------const keyword---------------
+
+//constant change nhi hota
+
+//constant declare
+
+// const pi = 3.14;
+// pi = 3.09; //assinging to a constant
+// console.log(pi);
+
+//contant ko  hum  ek baar value dene  ke baad dobara value nhi de sakte hai but contant ko  value ko hum use kar sakte hai
