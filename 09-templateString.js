20ae455f775f93adabe45647c72995ad028fe6e8
saif120991 <saif120991@gmail.com>
2023-09-07 17:43:17 +0530

    js notes

---
+++ 09-templateString.js
@@ -0,0 +1,19 @@
+//-----------template string--------------
+
+//without using template string
+// let age = 19;
+// let fullName = "Edward";
+
+//want to print 
+//my name is  edward and my age is 19
+
+// let aboutMe = "my name is" +" "+ fullName + "and my age is" +" "+ age ;
+// console.log(aboutMe);
+
+//-----------using template string---------
+
+//using backticks instead of using "double quotes",'single quotes'${placeholder},janha pe  value hame variable se chahiye usse hum dollar($) ke  placeholder me dalte hai
+
+// let aboutMe2 = `my name is ${fullName} and my age is ${age}`;
+// console.log(aboutMe2);
+
