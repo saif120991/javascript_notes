4a9be4c8b414709e2cd0ff4d7c94e0cb9eb221ba
saif120991 <saif120991@gmail.com>
2023-09-07 17:17:08 +0530

    js notes

---
+++ 08-stringConcatenation.js
@@ -0,0 +1,30 @@
+//---------------string concatenation-------------
+
+//concat means add karna (jodna)
+
+// let firstName = "mak";
+// let lastName = "soft";
+//agar mujhe space dena ho 2 strings ke beech to mujhe ek  blank string bhi beech me add karni hogi tab space aa jayega
+
+// ex:-
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+//ex2:-
+// let num1 = "15";
+// let num2 = "25";
+// let result = num1 + num2;
+// console.log(result); //result comes 1525
+
+//agar mujhe addition karna hai to mujhe inhe convert karna hoga number me
+
+//string ke  aage +"25" symbol add karke hum usse easily number me convert kar sakte hai
+
+//ex3:-
+// num1 = "15";
+// num2 = "35";
+// let result1 = +num1 + +num2;
+
+// console.log(result1);
+// console.log(typeof result1);
+
