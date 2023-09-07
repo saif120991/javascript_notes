6c2b8db1ad03990a7f97d3be2d7d1fae106a0555
saif120991 <saif120991@gmail.com>
2023-09-07 19:02:13 +0530

    notes

--- 07-typeofoperator_primitiveDataTypes_convertNumberToString_convertStringToNumber.js
+++ 07-typeofoperator_primitiveDataTypes_convertNumberToString_convertStringToNumber.js
@@ -13,29 +13,51 @@
 
 //typeof operator is used to check type of data
 
-//string
+//----------------string----------------
 // let mystr = "detective";
 
 // console.log(typeof mystr);
 // console.log(typeof "hello");
 
-//number
+//--------------number------------
 // let num1 = 98;
 // console.log(typeof num1);
 // console.log(typeof 45);
 
-//boolean
+//------------boolean-------------
 // let lionExistsInTheWorld = true;
 // console.log(typeof lionExistsInTheWorld);
 // console.log(typeof true);
 
-//undefined
-// let something; //variable declared only and value not assigned 
-// console.log(typeof something);
+//---------------undefined---------------
+// let something; //variable banaya sirf usko koi value assign nhi ki usse undefeined kehte hai
+// console.log(typeof something,something);
 
-//null
-// let khalidabba = null; //type is showing object
+//--------------null-------------
+//null ka matlab kuch nhi 
+// let khalidabba = null; //type is showing object jo ki ek bug/error hai
 // console.log(typeof khalidabba);
+
+//------------------BigInt------------
+//in js number ki ek limit hoti hai usse bada number store nhi kar sakte hum number data type me 
+
+//kitna bada number hum store kar sakte hai usse pata karne ke lie 
+
+//console.log(Number.MAX_SAFE_INTEGER);
+//9007199254740991 //itna bada
+
+//isse bada bhi number agar hame store karana ho tab BigInt use karte hai, chote numbers ke lie bhi hum BigInt use kar sakte hai , jo bhi operations hum BigInt ke sath use karte hai wo sirf BigInt ke sath he use hota hai other data type ke sath nhi hoga 
+
+//BigInt 2 tarike se hum likh sakte hai
+//1) let num = 235n;
+//number likh ke last me n laga do
+//BigInt like le round braces ke andar number dedo
+
+//2) let num2 = BigInt(50);
+    // let num3 = 70n;
+    // let result = num2 +  num3
+    // console.log(result);
+     
 
 
 //-------------------convert number to string---------------
