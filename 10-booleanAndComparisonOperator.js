0b29cbcbc15cab2431ba78af7d7df7e5f64c2c56
saif120991 <saif120991@gmail.com>
2023-09-08 22:16:49 +0530

    notes

---
+++ 10-booleanAndComparisonOperator.js
@@ -0,0 +1,30 @@
+
+//------------booleans and comparisonn operator-------------
+
+//booleans ya toh true hota hai ya false ye 2 he value  hoti hai boolean me
+
+
+// let num1 = 50;
+// let num2  = "50";
+// console.log(num1 < num2 );
+// console.log(num1 <= num2 );
+// console.log(num1 >= num2 );
+// console.log(num1 > num2 );
+
+// == vs ===
+// == double equal to sirf value check karta hai data type check  nhi karta hai , aur === triple equal to data type value dono ki equality check karta hai
+
+// console.log(num1 == num2 );
+// console.log(num1 === num2 );
+
+
+// != vs !==
+// console.log(num1 != num2 );
+// console.log(num1 !== num2 );
+
+//!= not equal to value check  karta hai , !== not double equal to value and data  type dono check karta hai
+
+
+
+
+
