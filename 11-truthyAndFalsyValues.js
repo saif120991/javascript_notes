23806c02e06c3c5ceb5c440a566badb91a0906cc
saif120991 <saif120991@gmail.com>
2023-09-09 12:03:07 +0530

    notes

---
+++ 11-truthyAndFalsyValues.js
@@ -0,0 +1,66 @@
+//------------falsy values--------------
+
+
+//false
+//""
+//null
+//undefined
+//0
+
+
+
+
+//--------------""----------------
+// let num  = "";
+// if(num){
+//     console.log(
+//         "true"
+//     );
+// }else{
+//     console.log("false");
+// }
+
+
+// //--------------null----------------
+// let num  = null;
+// if(num){
+//     console.log(
+//         "true"
+//     );
+// }else{
+//     console.log("false");
+// }
+
+
+// //--------------undefined----------------
+// let num;
+// if(num){
+//     console.log(
+//         "true"
+//     );
+// }else{
+//     console.log("false");
+// }
+
+
+// //--------------0----------------
+// let num=0;
+// if(num){
+//     console.log(
+//         "true"
+//     );
+// }else{
+//     console.log("false");
+// }
+
+
+
+//--------------false---------------
+// let num = false;
+// if(num){
+//     console.log(
+//         "true"
+//     );
+// }else{
+//     console.log("false");
+// }
---
+++ 12-ifelse.js
@@ -0,0 +1,18 @@
+//if else condition
+
+// let votingAge = 19;
+
+// if(votingAge >= 18){
+//     console.log("you can vote");
+// }else{
+//     console.log("you cannot vote");
+// }
+
+// let num = 22;
+
+// if(num%2 === 0){
+//     console.log("number is even");
+// }else{
+//     console.log("number is odd");
+// }
+
