7132e04131ea5a4bb5bebb157923eab3c69aaa0a
saif120991 <saif120991@gmail.com>
2023-09-09 12:27:45 +0530

    notes

---
+++ 13-ternaryOperatorOrConditionalOperator.js
@@ -0,0 +1,22 @@
+//------------without ternary or conditional operator------------
+
+// let age = 4;
+// let drink;
+
+// if(age >= 5){
+//     drink = "coffee";
+// }else{
+//     drink = "milk"
+// };
+// console.log(drink);
+
+// ------------with using ternary or conditional operator----------
+
+
+
+// let age = 2;
+//let variableName = condition ? true : false;
+//   let drink = age>=5 ? "coffee" : "milk";
+// console.log(drink);
+
+
