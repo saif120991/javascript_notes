4335cdfa8ed8e8736a0d84c6dbcec4a889c73eef
saif120991 <saif120991@gmail.com>
2023-09-11 18:04:34 +0530

    notes

---
+++ 20-breakAndContinueKeyword.js
@@ -0,0 +1,19 @@
+// //break keyword
+// for(let i = 0;i<=10;i++){
+//     if(i===4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//break keyword ke baad loop nhi chalta hai 
+
+//continue keyword
+for(let i = 0;i<=10;i++){
+    if(i===4){
+        continue;
+    }
+    console.log(i);
+}
+
+//continue keyword  me jis cheez ke lie condition lagi hai wo cheez print nhi hogi baki loop aage ki cheeze print hogi
