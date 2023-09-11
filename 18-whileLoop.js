cbb4d448efb33d67733584e681c894159e337d2a
saif120991 <saif120991@gmail.com>
2023-09-11 15:32:14 +0530

    notes

---
+++ 18-whileLoop.js
@@ -0,0 +1,52 @@
+//dry (don't repeat yourself)
+//print 1 2 3 by using whileloop 
+// let i = 0; //1 2 3 4
+
+// while (i<=3) {
+//     console.log(i);
+//     i++;
+// }
+// console.log(`current value of i is ${i}`);
+
+//while loop example
+//task:- print 1+2+3+4+5+6+7+8+9+10 
+
+//without using while loop
+//  let total = 0;
+
+//  total = total + 1;
+//  total = total + 2;
+//  total = total + 3;
+//  total = total + 4;
+//  total = total + 5;
+//  total = total + 6;
+//  total = total + 7;
+//  total = total + 8;
+//  total = total + 9;
+//  total = total + 10;
+
+// console.log(total);
+
+
+//by using while loop
+
+//
+
+// let total = 0;
+// let i = 0;
+// while(i<=10){
+//     total = total + i;
+//     i++;
+// }
+// console.log(total);
+
+
+
+ //by using formulae method
+ //formula :- (n(n+1))/2
+
+//  let num =  10;
+  
+//  let total =  num*(num+1)/2;
+//  console.log(total);
+
