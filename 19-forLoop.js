b33a4c45ba726de1408388b98ecdef755e5eb10e
saif120991 <saif120991@gmail.com>
2023-09-11 17:31:31 +0530

    notes

---
+++ 19-forLoop.js
@@ -0,0 +1,15 @@
+//print 1 2 3 4 5 6 7 8 9 10
+
+// for(let i = 0;i<10;i++){
+//     console.log(i);
+// };
+
+//for loop example:-
+//print first 10 natural numbers sum
+
+let total = 0;
+
+for (let i=0; i<=10; i++){
+    total = total + i
+}
+console.log(total);
